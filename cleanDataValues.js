6b9c706aa9aaaeedc88a0eee19130f7d5e543e4e
Taylor Downs <downs.taylor@gmail.com>
2020-03-05 14:08:12 +0000

    add more logs

--- cleanDataValues.js
+++ cleanDataValues.js
@@ -1,16 +1,25 @@
 alterState(state => {
-  const cleanSet = state.data.dataValues.map(x => {
-    if (x.value > 1000) {
-      // NOTE: Entries must be in single units, we specified thousands.
-      console.log(
-        'Found value reported in single units: ' + JSON.stringify(x, null, 2)
-      );
-      x.value = x.value / 1000;
-      console.log('Converted to thousands.');
+  const cleanSet = state.data.dataValues
+    .map(x => {
+      if (x.value > 1000) {
+        // NOTE: Entries must be in single units, we specified thousands.
+        console.log(
+          'Found value reported in single units: ' + JSON.stringify(x, null, 2)
+        );
+        x.value = x.value / 1000;
+        console.log('Converted to thousands.');
+        return x;
+      }
+      console.log('No incorrect units in import.');
       return x;
-    }
-    return x;
-  });
+    })
+    .map(x => {
+      if (x.value > 20) {
+        console.log('Large value found: ' + JSON.stringify(x, null, 2));
+        return x;
+      }
+      return x;
+    });
   state.data.dataValues = cleanSet;
   return state;
 });
