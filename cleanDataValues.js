d6e22930c9eaa297a3fe20e19ea35ee86ec32e4d
Taylor Downs <downs.taylor@gmail.com>
2020-03-05 14:02:46 +0000

    log cleanup

--- cleanDataValues.js
+++ cleanDataValues.js
@@ -1,8 +1,12 @@
 alterState(state => {
   const cleanSet = state.data.dataValues.map(x => {
     if (x.value > 1000) {
-      // Entries must be in single units, we specified thousands.
+      // NOTE: Entries must be in single units, we specified thousands.
+      console.log(
+        'Found value reported in single units: ' + JSON.stringify(x, null, 2)
+      );
       x.value = x.value / 1000;
+      console.log('Converted to thousands.');
       return x;
     }
     return x;
