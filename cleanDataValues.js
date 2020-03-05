1e00ee42d6bbb8131da634d46ce3f78dbf83f598
Taylor Downs <downs.taylor@gmail.com>
2020-03-05 13:59:39 +0000

    fake cleaning

--- cleanDataValues.js
+++ cleanDataValues.js
@@ -1,4 +1,12 @@
 alterState(state => {
-  
+  const cleanSet = state.data.dataValues.map(x => {
+    if (x.value > 1000) {
+      // Entries must be in single units, we specified thousands.
+      x.value = x.value / 1000;
+      return x;
+    }
+    return x;
+  });
+  state.data.dataValues = cleanSet;
   return state;
 });
